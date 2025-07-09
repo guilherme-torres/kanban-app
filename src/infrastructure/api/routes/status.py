c7d3851af1083d5b44fc5a3b6e0dc1c0db1acdd9
guilherme-torres <guilhermetorres1401@gmail.com>
2025-07-09 16:27:17 -0300

    add status api routes

---
+++ src/infrastructure/api/routes/status.py
@@ -0,0 +1,44 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from src.adapters.controllers.status_controller import StatusController
+from src.core.use_cases.create_status import CreateStatusUseCase
+from src.core.use_cases.delete_status import DeleteStatusUseCase
+from src.core.use_cases.exceptions import UserNotFoundError, StatusNotFoundError
+from src.infrastructure.persistence.repositories.status_repository import StatusRepository
+from src.infrastructure.persistence.repositories.user_repository import UserRepository
+from src.infrastructure.persistence.database.db import get_db_session
+from src.adapters.schemas.status import StatusCreate, StatusResponse
+
+
+router = APIRouter(prefix="/status")
+
+
+def get_status_controller(db: Session):
+    status_repository = StatusRepository(db)
+    user_repository = UserRepository(db)
+    create_status_use_case = CreateStatusUseCase(
+        status_repository=status_repository, user_repository=user_repository
+    )
+    delete_status_use_case = DeleteStatusUseCase(status_repository)
+    return StatusController(create_status_use_case, delete_status_use_case)
+
+
+@router.post("/", response_model=StatusResponse)
+def create_status(status_data: StatusCreate, db: Session = Depends(get_db_session)):
+    try:
+        status_controller = get_status_controller(db)
+        return status_controller.create_status(status_data)
+    except UserNotFoundError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="User not found"
+        )
+    
+@router.delete("/{id}")
+def delete_status(id: int, db: Session = Depends(get_db_session)):
+    try:
+        status_controller = get_status_controller(db)
+        return status_controller.delete_status(id)
+    except StatusNotFoundError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Status not found"
+        )
--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
-from src.infrastructure.api.routes.user import router as user_router
 from src.infrastructure.persistence.database.db import Base, engine
+from src.infrastructure.api.routes.user import router as user_router
+from src.infrastructure.api.routes.status import router as status_router
 
 Base.metadata.create_all(engine)
 
 app = FastAPI()
 
 app.include_router(user_router)
+app.include_router(status_router)
 
 @app.get("/")
 def root():
